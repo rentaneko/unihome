d4f9add5b6a4bf8ecc12f376cce31c3778e95a50
DESKTOP-2F4J4R9\Acer <len.kagamine181998@gmail.com>
2023-03-01 20:15:04 +0700

    - update

--- lib/views/profile/profile.view.dart
+++ lib/views/profile/profile.view.dart
@@ -9,28 +9,31 @@ class ProfileScreen extends GetWidget<ProfileController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: controller.isLoading.value
-          ? const Center(child: CircularProgressIndicator.adaptive())
-          : Column(
-              children: [
-                Text('[ID] ========== ${controller.renter.value.id}'),
-                Text(
-                    '[USERNAME] ========== ${controller.renter.value.username}'),
-                Text('[EMAIL] ========== ${controller.renter.value.email}'),
-                Text('[PHONE] ========== ${controller.renter.value.phone}'),
-                Text(
-                    '[FULLNAME] ========== ${controller.renter.value.fullname}'),
-                Text(
-                    '[BIRTHDATE] ========== ${controller.renter.value.birthdate}'),
-                Text('[STATUS] ========== ${controller.renter.value.status}'),
-                Text(
-                    '[CITIZENNUMBER] ========== ${controller.renter.value.citizenNumber}'),
-                Text(
-                    '[CITIZENIMAGE] ========== ${controller.renter.value.citizenImageUrl}'),
-                Text('[ADDRESS] ========== ${controller.renter.value.address}'),
-                Text('[GENDER] ========== ${controller.renter.value.gender}'),
-              ],
-            ),
+      body: Obx(
+        () => controller.isLoading.value
+            ? const Center(child: CircularProgressIndicator.adaptive())
+            : Column(
+                children: [
+                  Text('[ID] ========== ${controller.renter.value.id}'),
+                  Text(
+                      '[USERNAME] ========== ${controller.renter.value.username}'),
+                  Text('[EMAIL] ========== ${controller.renter.value.email}'),
+                  Text('[PHONE] ========== ${controller.renter.value.phone}'),
+                  Text(
+                      '[FULLNAME] ========== ${controller.renter.value.fullname}'),
+                  Text(
+                      '[BIRTHDATE] ========== ${controller.renter.value.birthdate}'),
+                  Text('[STATUS] ========== ${controller.renter.value.status}'),
+                  Text(
+                      '[CITIZENNUMBER] ========== ${controller.renter.value.citizenNumber}'),
+                  Text(
+                      '[CITIZENIMAGE] ========== ${controller.renter.value.citizenImageUrl}'),
+                  Text(
+                      '[ADDRESS] ========== ${controller.renter.value.address}'),
+                  Text('[GENDER] ========== ${controller.renter.value.gender}'),
+                ],
+              ),
+      ),
     );
   }
 }
